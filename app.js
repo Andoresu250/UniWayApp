402b3fff310511b5b6310e2c68a66eb57ef09731
Andoresu250 <andres-vera95hotmail.com>
2015-11-12 11:09:03 -0500

    algo

--- app.js
+++ app.js
@@ -4,20 +4,34 @@ angular.module('Uniway', ['ngRoute'])
 	$routeProvider
 	
 	.when('/home',{
+		
 		templateUrl: 'client/partials/signIn.html',
 		controller: 'signInCtrl'
+
 	})
 	.when('/signUp', {
+		
 		templateUrl: 'client/partials/signUp.html',	
 		controller: 'signUpCtrl'	
+
 	})
 	.when('/dashboard', {
+		
 		templateUrl: 'client/partials/dashboard.html',		
 		controller: 'dashboardCtrl'
+
 	})
 	.when('/search', {
+		
 		templateUrl: 'client/partials/search.html',		
-		controller: 'dashboardCtrl'
+		controller: 'searchCtrl'
+
+	})
+	.when('/timetable', {
+
+		templateUrl: 'client/partials/timetable.html',		
+		controller: 'timetableCtrl'
+
 	})
 	.otherwise({ redirectTo: '/home' })
 }])
@@ -33,8 +47,48 @@ angular.module('Uniway', ['ngRoute'])
 	$scope.logo = imagesRoutes + "Uniway.png";
 	$scope.active = "1";
 	$scope.formSignUp = {};
-	$scope.gPlace;
+	$scope.gPlace;	
 	
+	$scope.isActive = function (id) {		
+		return $scope.active === id;
+	}
+
+	$scope.asignActive = function (id) {
+		$scope.active = id;
+	}
+	$scope.setRange = function(range){
+		$scope.formSignUp.rank=range;
+	}
+	$scope.addAddress = function(){
+		$scope.formSignUp.locationU=this.gPlace.getPlace().place_id;
+	}
+	$scope.signup = function(){
+		var form=$scope.formSignUp
+		var data ={
+			username: form.userName, 
+			password: form.password, 
+			password_confirmation: form.cpassword, 
+			rank: form.rank,    //'sa'/'admin'/'driver'/'passenger'
+			name: form.nameU, 
+			email: form.email, 
+			location: form.locationU, 
+			capacity: form.capacity
+		};
+		$http.post(url='http://uniway-api.herokuapp.com/users',data).then(function(data){
+			$scope.asignActive('6');
+		},function(error){
+			alert('No se puedo registrar')
+		});
+	
+	}
+}])
+.controller('searchCtrl', ['$scope', function ($scope) {
+	$('#select_uni').prop('checked',true);
+}])
+.controller('dashboardCtrl', ['$scope', function ($scope) {
+	
+}])
+.controller('timetableCtrl', ['$scope', function ($scope) {
 	$("input[name='mon_time_u']").val("06:30");
 	$("#default_time_u").click(function () {
 		var check = $("#default_time_u").is(':checked')		
@@ -65,44 +119,6 @@ angular.module('Uniway', ['ngRoute'])
 			$("input[name='fri_time_h']").val("");	
 		}		
 	});
-	$scope.isActive = function (id) {		
-		return $scope.active === id;
-	}
-
-	$scope.asignActive = function (id) {
-		$scope.active = id;
-	}
-	$scope.setRange = function(range){
-		$scope.formSignUp.rank=range;
-	}
-	$scope.addAddress = function(){
-		$scope.formSignUp.locationU=this.gPlace.getPlace().place_id;
-	}
-	$scope.signup = function(){
-		var form=$scope.formSignUp
-		var data ={
-			username: form.userName, 
-			password: form.password, 
-			password_confirmation: form.cpassword, 
-			rank: form.rank,    //'sa'/'admin'/'driver'/'passenger'
-			name: "form.nameU", 
-			email: form.email, 
-			location: form.locationU, 
-			capacity: form.capacity
-		};
-		$http.post(url='http://uniway-api.herokuapp.com/users',data).then(function(data){
-			$scope.asignActive('6');
-		},function(error){
-			alert('No se puedo registrar')
-		});
-	
-	}
-}])
-.controller('searchCtrl', ['$scope', function ($scope) {
-	
-}])
-.controller('dashboardCtrl', ['$scope', function ($scope) {
-	
 }])
 .directive('navbar', function() {
   return {
@@ -111,6 +127,15 @@ angular.module('Uniway', ['ngRoute'])
     link: function(scope, elem, attrs){
     	$(".button-collapse").sideNav();
     	$('.button-collapse').sideNav('hide');
+    }
+  };
+})
+.directive('selectlist', function() {
+  return {
+    restrict: 'E',
+    templateUrl: 'client/partials/directives/selectlist.html',
+    link: function(scope, elem, attrs){
+    	$('select').material_select();
     }
   };
 })
@@ -126,10 +151,10 @@ angular.module('Uniway', ['ngRoute'])
 
             google.maps.event.addListener(scope.gPlace, 'place_changed', function() {
                 scope.$apply(function() {
-                    model.$setViewValue(element.val());                
+                    model.$setViewValue(scope.gPlace.getPlace().place_id);                
                 });
             });
         }
     };
-});
+})
 ;
